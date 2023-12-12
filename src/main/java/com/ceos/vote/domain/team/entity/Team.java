1723390cdc2027d94e4b9145be01fc35acffec1b
gkim <kimkm0419@naver.com>
2023-12-12 16:38:55 +0900

    :sparkles: feat: Create team domain
    - Team 엔티티 생성
    - Team 리포지토리 생성
    
    Related: #1

---
+++ src/main/java/com/ceos/vote/domain/team/entity/Team.java
@@ -0,0 +1,24 @@
+package com.ceos.vote.domain.team.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "team")
+public class Team {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+}
---
+++ src/main/java/com/ceos/vote/domain/team/repository/TeamRepository.java
@@ -0,0 +1,9 @@
+package com.ceos.vote.domain.team.repository;
+
+import com.ceos.vote.domain.team.entity.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Long> {
+}
