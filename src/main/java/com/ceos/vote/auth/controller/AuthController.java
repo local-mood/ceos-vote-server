e29b8a4fc40693eaffff5e92dbb43d32807e4a20
yeni-choi <yewonchoi@ewhain.net>
2023-12-13 20:30:41 +0900

    :sparkles: feat: create Token validate & reissue in auth controller
    Related: #10

--- src/main/java/com/ceos/vote/auth/controller/AuthController.java
+++ src/main/java/com/ceos/vote/auth/controller/AuthController.java
@@ -46,5 +46,47 @@ public class AuthController {
                 .build();
     }
 
+    // 토큰 유효성 검사
+    @PostMapping("/validate")
+    public ResponseEntity<?> validate(@RequestHeader("Authorization") String requestAccessToken) {
+        if (!authService.validate(requestAccessToken)) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+    // 토큰 재발급
+    @PostMapping("/reissue")
+    public ResponseEntity<?> reissue(@CookieValue(name = "refresh-token") String requestRefreshToken,
+                                     @RequestHeader("Authorization") String requestAccessToken) {
+
+        TokenDto newAuthToken = authService.reissue(requestAccessToken, requestRefreshToken);
+
+        if (newAuthToken != null) {
+            // 새로운 토큰 발급, 반환
+            ResponseCookie responseCookie = ResponseCookie.from("refresh-token", newAuthToken.getRefreshToken())
+                    .maxAge(COOKIE_EXPIRATION)
+                    .httpOnly(true)
+                    .secure(true)
+                    .sameSite(Cookie.SameSite.NONE.attributeValue())
+                    .build();
+            return ResponseEntity.status(HttpStatus.OK)
+                    .header(HttpHeaders.SET_COOKIE, responseCookie.toString())
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + newAuthToken.getAccessToken())
+                    .build();
+        } else {
+            //  Refresh Token이 탈취 가능할 때 쿠키 삭제하고 재로그인
+            ResponseCookie responseCookie = ResponseCookie.from("refresh-token", "")
+                    .maxAge(0)
+                    .path("/")
+                    .build();
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .header(HttpHeaders.SET_COOKIE, responseCookie.toString())
+                    .build();
+        }
+    }
+
 
 }
