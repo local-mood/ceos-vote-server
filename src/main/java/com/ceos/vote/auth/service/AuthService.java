5d3a2840950fe232bf266cd10a3d92f7436805db
yeni-choi <yewonchoi@ewhain.net>
2023-12-13 20:15:46 +0900

    :wrench: feat: create Signup API in auth service
    Related: #10

---
+++ src/main/java/com/ceos/vote/auth/service/AuthService.java
@@ -0,0 +1,57 @@
+package com.ceos.vote.auth.service;
+
+import com.ceos.vote.auth.jwt.entity.TokenDto;
+import com.ceos.vote.exception.ErrorCode;
+import com.ceos.vote.exception.CeosException;
+import com.ceos.vote.auth.jwt.provider.JwtTokenProvider;
+import com.ceos.vote.auth.jwt.entity.LoginRequestDto;
+import com.ceos.vote.domain.member.dto.MemberRequestDto;
+import com.ceos.vote.domain.member.entity.Member;
+import com.ceos.vote.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.stream.Collectors;
+
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AuthService {
+
+    private final AuthenticationManagerBuilder authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final RedisService redisService;
+    private final PasswordEncoder passwordEncoder;
+
+    private final MemberRepository memberRepository;
+    private final String SERVER = "Server";
+
+    // 회원가입
+    @Transactional
+    public void joinMember(MemberRequestDto requestDto) {
+
+        // 이메일, ID 중복 검사
+        if (findUserByEmail(requestDto.getEmail()))
+            throw new CeosException(ErrorCode.ALREADY_MEMBER_EMAIL);
+
+        if (findUserByUserid(requestDto.getUserid()))
+            throw new CeosException(ErrorCode.ALREADY_MEMBER_ID);
+
+        Member member = requestDto.toMember(passwordEncoder);
+        memberRepository.save(member);
+    }
+
+
+
+    public boolean findUserByEmail(String email) { return memberRepository.existsByEmail(email);}
+    public boolean findUserByUserid(String userid) { return memberRepository.existsByUserid(userid);}
+
+}
