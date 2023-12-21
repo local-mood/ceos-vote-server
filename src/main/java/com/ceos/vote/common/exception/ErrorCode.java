f93bd51bebee2d395d1a4eb5b04be366296e6e13
gkim <kimkm0419@naver.com>
2023-12-21 22:42:04 +0900

    :sparkles: feat: Add new error code
    - 투표 완료 유저 에러 코드 추가
    
    Related: #24

--- src/main/java/com/ceos/vote/common/exception/ErrorCode.java
+++ src/main/java/com/ceos/vote/common/exception/ErrorCode.java
@@ -12,7 +12,8 @@ public enum ErrorCode {
     ALREADY_MEMBER_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 유저 정보입니다.", "다른 이메일로 가입해주세요."),
     ALREADY_MEMBER_ID(HttpStatus.CONFLICT, "이미 존재하는 유저 정보입니다.", "다른 ID로 가입해주세요."),
     INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "아이디 혹은 비밀번호가 틀렸습니다.", "다시 시도해주세요."),
-    INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 액세스 토큰입니다.", "다시 로그인해주세요.");
+    INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 액세스 토큰입니다.", "다시 로그인해주세요."),
+    ALREADY_VOTED_USER(HttpStatus.BAD_REQUEST, "이미 투표를 완료한 유저입니다.", "투표가 불가합니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
