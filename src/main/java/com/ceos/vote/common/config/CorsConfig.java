728e8089f2270e7988a0b63081d3ebce7a1e5714
gkim <kimkm0419@naver.com>
2023-12-23 17:20:02 +0900

    :ambulance: fix: Add cors allowed-origin
    - allowed-origin 도메인 추가
    
    Related: #44

--- src/main/java/com/ceos/vote/common/config/CorsConfig.java
+++ src/main/java/com/ceos/vote/common/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.ceos.vote.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,12 +10,15 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
+  @Value("${domain}")
+  private String domain;
+
   @Bean
   public CorsFilter corsFilter() {
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     CorsConfiguration config = new CorsConfiguration();
     config.setAllowCredentials(true);
-    config.addAllowedOrigin("*");
+    config.addAllowedOrigin(domain);
     config.addAllowedHeader("*");
     config.addAllowedMethod("*");
     source.registerCorsConfiguration("/**",config);
